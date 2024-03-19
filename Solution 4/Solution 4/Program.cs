5aecfc45f63037003b2dcdf07d6d57e451f7a701
s28876 <kaczorekt6@gmail.com>
2024-03-19 10:55:59 +0100

    Initial Message

--- Solution 4/Solution 4/Program.cs
+++ Solution 4/Solution 4/Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+public class Con
+{
+    private int weight { get; set; }
+    private int height { get; set; }
+    public int cargo_weight { get; set; }
+    private int deep { get; set; }
+    private String serial { get; set; }
+    private static int num = 0;
+    private int max_load { get; set; }
+
+    public Con(int weight, int height, int cargoWeight, int deep, int max_load)
+    {
+        this.weight = weight;
+        this.height = height;
+        this.cargo_weight = cargoWeight;
+        this.deep = deep;
+        num++;
+        serial = "KON-" + "C-" + num.ToString(); 
+        this.max_load = max_load;
+    }
+
+    
+}
+
+
