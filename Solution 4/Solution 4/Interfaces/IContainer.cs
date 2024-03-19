d87acd7bd12ea906aec211f843600ee40f114056
s28876 <kaczorekt6@gmail.com>
2024-03-19 11:26:12 +0100

    Initial Commit

---
+++ Solution 4/Solution 4/Interfaces/IContainer.cs
@@ -0,0 +1,10 @@
+﻿using Solution_4.Exceptions;
+
+namespace Solution_4;
+
+public interface IContainer
+{
+    void Unload();
+
+    void Load(double cargoWeight);
+}
--- Solution 4/Solution 4/Program.cs
+++ Solution 4/Solution 4/Program.cs
@@ -1,18 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Solution_4;
+using Solution_4.Exceptions;
 
-public class Con
+
+public abstract class Con : IContainer
 {
     private int weight { get; set; }
     private int height { get; set; }
-    public int cargo_weight { get; set; }
+    public int cargoWeight { get; set; }
     private int deep { get; set; }
     private String serial { get; set; }
     private static int num = 0;
-    private int max_load { get; set; }
+    private int maxLoad { get; set; }
+    public virtual void Load(double cargoWeight)
+    {
+        throw new OverfillException();
+    }
 
-    
+    public virtual void Unload()
+    {
+        throw new OverfillException();
+    }
+
+    protected Con(int weight, int height, int cargoWeight, int deep, int maxLoad)
+    {
+        this.weight = weight;
+        this.height = height;
+        this.cargoWeight = cargoWeight;
+        this.deep = deep;
+        num++;
+        serial = "KON-" + "C-" + num.ToString(); 
+        this.maxLoad = maxLoad;
+    }
 
     
 }
